5be8f4c1381c49ace8399d502dde9ea7d312fa4e
cgd_office_pc <349008059@qq.com>
2020-11-12 14:16:29 +0800

    first-commit

---
+++ config/wechat.js
@@ -0,0 +1,8 @@
+const wechatConfig = {
+  domain: 'http://192.168.2.137:8080',
+  expiresIn: 7200,
+  appid: 'wx45e6f7f8ff25c743',
+  secret: 'fa537adbbef071a579951a43d2dbbbc4'
+}
+
+module.exports = wechatConfig
